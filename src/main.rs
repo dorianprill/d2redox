7d40346b2bb7ad53226c5ab90f88c0ca1666614c
dorianprill <dorian.prill@gmail.com>
2019-01-14 16:51:40 +0100

    remove unnecessary packet handler functions

--- src/main.rs
+++ src/main.rs
@@ -30,77 +30,7 @@ use std::io::{self, Write};
 use std::net::IpAddr;
 use std::process;
 
-fn handle_udp_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
-    let udp = UdpPacket::new(packet);
 
-    if let Some(udp) = udp {
-        println!(
-            "[{}]: UDP Packet: {}:{} > {}:{}; length: {}",
-            interface_name,
-            source,
-            udp.get_source(),
-            destination,
-            udp.get_destination(),
-            udp.get_length()
-        );
-    } else {
-        println!("[{}]: Malformed UDP Packet", interface_name);
-    }
-}
-
-fn handle_icmp_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
-    let icmp_packet = IcmpPacket::new(packet);
-    if let Some(icmp_packet) = icmp_packet {
-        match icmp_packet.get_icmp_type() {
-            IcmpTypes::EchoReply => {
-                let echo_reply_packet = echo_reply::EchoReplyPacket::new(packet).unwrap();
-                println!(
-                    "[{}]: ICMP echo reply {} -> {} (seq={:?}, id={:?})",
-                    interface_name,
-                    source,
-                    destination,
-                    echo_reply_packet.get_sequence_number(),
-                    echo_reply_packet.get_identifier()
-                );
-            }
-            IcmpTypes::EchoRequest => {
-                let echo_request_packet = echo_request::EchoRequestPacket::new(packet).unwrap();
-                println!(
-                    "[{}]: ICMP echo request {} -> {} (seq={:?}, id={:?})",
-                    interface_name,
-                    source,
-                    destination,
-                    echo_request_packet.get_sequence_number(),
-                    echo_request_packet.get_identifier()
-                );
-            }
-            _ => println!(
-                "[{}]: ICMP packet {} -> {} (type={:?})",
-                interface_name,
-                source,
-                destination,
-                icmp_packet.get_icmp_type()
-            ),
-        }
-    } else {
-        println!("[{}]: Malformed ICMP Packet", interface_name);
-    }
-}
-
-fn handle_icmpv6_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
-    let icmpv6_packet = Icmpv6Packet::new(packet);
-    if let Some(icmpv6_packet) = icmpv6_packet {
-        println!(
-            "[{}]: ICMPv6 packet {} -> {} (type={:?})",
-            interface_name,
-            source,
-            destination,
-            icmpv6_packet.get_icmpv6_type()
-        )
-    } else {
-        println!("[{}]: Malformed ICMPv6 Packet", interface_name);
-    }
-}
 
 fn handle_tcp_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
     let tcp = TcpPacket::new(packet);
@@ -184,22 +114,6 @@ fn handle_ipv6_packet(interface_name: &str, ethernet: &EthernetPacket) {
     }
 }
 
-fn handle_arp_packet(interface_name: &str, ethernet: &EthernetPacket) {
-    let header = ArpPacket::new(ethernet.payload());
-    if let Some(header) = header {
-        println!(
-            "[{}]: ARP packet: {}({}) > {}({}); operation: {:?}",
-            interface_name,
-            ethernet.get_source(),
-            header.get_sender_proto_addr(),
-            ethernet.get_destination(),
-            header.get_target_proto_addr(),
-            header.get_operation()
-        );
-    } else {
-        println!("[{}]: Malformed ARP Packet", interface_name);
-    }
-}
 
 fn handle_ethernet_frame(interface: &NetworkInterface, ethernet: &EthernetPacket) {
     let interface_name = &interface.name[..];
